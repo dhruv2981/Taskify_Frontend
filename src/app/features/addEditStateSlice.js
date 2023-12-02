7109064c23e2a6e30f146b628735af3839dde735
dhruv2981 <dhruvmanchanda040@gmail.com>
2023-12-02 12:00:35 +0530

    logut api written

---
+++ src/app/features/addEditStateSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const addEditStateSlice = createSlice({
+  name: "addEdit",
+  initialState: {
+    addEditState: {
+      editList: false,
+      addList:false,
+      editListId: "",
+      editCard:false,
+      editCardId:'',
+    },
+    loading: "idle",
+    error: "Lists could not be retreived",
+  },
+  reducers: {
+    updateAddEditState: (state, action) => {
+      const { newAddEditState } = action.payload;
+      state.addEditState = newAddEditState;
+      console.log(state.addEditState,"stage 2");
+    },
+  },
+});
+
+export const selectAddEdit = (state) => state.addEdit.addEditState;
+export const { updateAddEditState } = addEditStateSlice.actions;
+export default addEditStateSlice.reducer;
